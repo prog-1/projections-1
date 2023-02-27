91ecbda5b849f7a0dcb59415c50138a284ce3dd1
Aleksandrs Stukalovs <astukalovs@edu.riga.lv>
2023-02-27 10:26:26 +0200

    Implemented Y-axis rotation

--- CentralProjection/main.go
+++ CentralProjection/main.go
@@ -38,33 +38,44 @@ type Rotator struct {
 	X, Y, Z float64
 }
 
+func (v *Vec) RotateZ(rad float64) {
+	x := v.X*math.Cos(rad) - v.Y*math.Sin(rad)
+	y := v.X*math.Sin(rad) + v.Y*math.Cos(rad)
+	v.X, v.Y = x, y
+}
+
+func (v *Vec) RotateY(rad float64) {
+	x := v.X*math.Cos(rad) + v.Z*math.Sin(rad)
+	z := v.X*-math.Sin(rad) + v.Z*math.Cos(rad)
+	v.X, v.Z = x, z
+}
+
+func (v *Vec) RotateX(rad float64) {
+	y := v.Y*math.Cos(rad) - v.Z*math.Sin(rad)
+	z := -v.Y*math.Sin(rad) + v.Z*math.Cos(rad)
+	v.Y, v.Z = y, z
+}
+
 func (v *Vec) Rotate(rad Rotator) {
-	// Rotation around Z
-	v.X = v.X*math.Cos(rad.Z) - v.Y*math.Sin(rad.Z)
-	v.Y = v.X*math.Sin(rad.Z) + v.Y*math.Cos(rad.Z)
-
-	// Rotation around Y
-	v.X = v.X*math.Cos(rad.Y) - v.Z*math.Sin(rad.Y)
-	v.Z = v.X*math.Sin(rad.Y) + v.Z*math.Cos(rad.Y)
-
-	// Rotation around X
-	v.Y = v.Y*math.Cos(rad.X) - v.Z*math.Sin(rad.X)
-	v.Z = -v.Y*math.Sin(rad.X) + v.Z*math.Cos(rad.X)
+	v.RotateZ(rad.Z)
+	v.RotateY(rad.Y)
+	v.RotateX(rad.X)
 }
 
 func CentralProjection(v Vec, k float64) Vec {
 	return Vec{
 		-(v.X / v.Z) * k,
 		-(v.Y / v.Z) * k,
-		-k,
+		-1,
 	}
 }
 
 func DrawLine(screen *ebiten.Image, a, b Vec, clr color.Color) {
-	k := float64(-250)
+	halfWidth, halfHeight := float64(screenWidth/2), float64(screenHeight/2)
+	k := float64(250)
 	a = CentralProjection(a, k)
 	b = CentralProjection(b, k)
-	ebitenutil.DrawLine(screen, a.X, a.Y, b.X, b.Y, color.RGBA{255, 102, 204, 255})
+	ebitenutil.DrawLine(screen, a.X+halfWidth, a.Y+halfHeight, b.X+halfWidth, b.Y+halfHeight, color.RGBA{255, 102, 204, 255})
 }
 
 type Rect struct {
@@ -117,35 +128,21 @@ type game struct {
 }
 
 func NewGame() *game {
-	halfWidth, halfHeight := float64(screenWidth/2), float64(screenHeight/2)
 	return &game{
 		[]Cube{
 			{
 				[8]Vec{
-					{-200 + halfWidth, -200 + halfHeight, 200}, // NearBottomLeft
-					{-200 + halfWidth, 200 + halfHeight, 200},  // NearTopLeft
-					{200 + halfWidth, 200 + halfHeight, 200},   // NearTopRight
-					{200 + halfWidth, -200 + halfHeight, 200},  // NearBottomRightS
+					{-200, -200, 200}, // NearBottomLeft
+					{-200, 200, 200},  // NearTopLeft
+					{200, 200, 200},   // NearTopRight
+					{200, -200, 200},  // NearBottomRightS
 
-					{-200 + halfWidth, -200 + halfHeight, 250}, // FarBottomLeft
-					{-200 + halfWidth, 200 + halfHeight, 250},  // FarTopLeft
-					{200 + halfWidth, 200 + halfHeight, 250},   // FarTopRight
-					{200 + halfWidth, -200 + halfHeight, 250},  // FarBottomRight
+					{-200, -200, 600}, // FarBottomLeft
+					{-200, 200, 600},  // FarTopLeft
+					{200, 200, 600},   // FarTopRight
+					{200, -200, 600},  // FarBottomRight
 				},
 			},
-			// {
-			// 	[8]Vec{
-			// 		{-400 + halfWidth, -200 + halfHeight, 200}, // NearBottomLeft
-			// 		{-400 + halfWidth, 200 + halfHeight, 200},  // NearTopLeft
-			// 		{halfWidth, 200 + halfHeight, 200},         // NearTopRight
-			// 		{halfWidth, -200 + halfHeight, 200},        // NearBottomRight
-
-			// 		{-400 + halfWidth, -200 + halfHeight, 600}, // FarBottomLeft
-			// 		{-400 + halfWidth, 200 + halfHeight, 600},  // FarTopLeft
-			// 		{halfWidth, 200 + halfHeight, 600},         // FarTopRight
-			// 		{halfWidth, -200 + halfHeight, 600},        // FarBottomRight
-			// 	},
-			// },
 		},
 		ebiten.NewImage(screenWidth, screenHeight),
 	}
@@ -154,7 +151,7 @@ func NewGame() *game {
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
 	for i := range g.c {
-		g.c[i].Rotate(g.screenBuffer, Rotator{0, 0, math.Pi / 180})
+		g.c[i].Rotate(g.screenBuffer, Rotator{0, math.Pi / 180, 0})
 	}
 	return nil
 }
